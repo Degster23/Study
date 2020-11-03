5c8a33977c6121c0cf136772681656d94f90c901
Romanchuk Dmitrii <dsr1503@gmail.com>
2020-11-03 10:13:31 +0600

    fib

--- Фибаначи/Form1.cs
+++ Фибаначи/Form1.cs
@@ -19,19 +19,34 @@ namespace Фибаначи
 
         public string fibanachi(string a)
         {
-            int kol = int.Parse(a) + 1;
-            double[] fib = new double[kol];
-            fib[1] = 1;
-            for (int i=2; i<kol; i++)
+            double[] fib = new double[int.Parse(a)];
+            fib[0] = 1;
+            if (fib.Length>1) fib[1] = 1;
+            for (int i=2; i < int.Parse(a); i++)
             {
                 fib[i] = fib[i - 2] + fib[i - 1];
             }
-
+            string itog = "";
+            for (int i=0; i<fib.Length; i++)
+            {
+                itog = itog + " " + fib[i].ToString();
+            }
+            return itog;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            fibanachi(textBox1.Text);
+            richTextBox1.Clear();
+            richTextBox1.Text = fibanachi(textBox1.Text);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
         }
     }
 }
