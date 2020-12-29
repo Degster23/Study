faef5e38c6ad4c5d864b56d0a3d52f095fd3a180
Degster23 <73150778+Degster23@users.noreply.github.com>
2020-12-29 06:12:03 +0300

    Ochered (Queue)

---
+++ Ochered (Queue)/Ochered (Queue)/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Ochered__Queue_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Ввод";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Enabled = false;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(12, 35);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Удаление";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите число";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Вывести в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Очистка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Добавить из файла";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(202, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(220, 187);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(400, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(22, 20);
+            this.button5.TabIndex = 11;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 236);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Очередь";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ochered (Queue)/Ochered (Queue)/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ochered__Queue_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = false;
+            radioButton3.Checked = false;
+        }
+
+        Queue<int> chisla = new Queue<int>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                bool success = int.TryParse(textBox1.Text, out int number);
+                if (success == true)
+                {
+                    chisla.Enqueue(number);
+                    textBox1.Clear();
+
+                    richTextBox1.Clear();
+                    int[] maschis = new int[chisla.Count];
+                    chisla.CopyTo(maschis, 0);
+                    for (int i = 0; i < (maschis.Length); i++)
+                    {
+                        richTextBox1.Text += (i + 1) + ". " + maschis[i].ToString() + Environment.NewLine;
+                    }
+                    radioButton3.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                }
+            }
+
+            if (radioButton3.Checked == true)
+            {
+                string del = "";
+
+                bool prov1 = int.TryParse(textBox1.Text, out int result1);
+
+                DialogResult yes = MessageBox.Show("Вы уверены, что хотите удалить данный элемент", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2, MessageBoxOptions.DefaultDesktopOnly);
+
+                if (yes == DialogResult.Yes)
+                {
+                    if (prov1 == true)
+                    {
+                        if ((result1 > 0) & (result1 <= chisla.Count))
+                        {
+                            DialogResult res = MessageBox.Show("Сохранить удаляемые элементы?", "Выбор", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                            if (res == DialogResult.Yes)
+                            {
+                                int i = 0;
+                                while (i != int.Parse(textBox1.Text))
+                                {
+                                    del += chisla.Dequeue().ToString() + Environment.NewLine;
+                                    i++;
+                                }
+
+                                SaveFileDialog sf = new SaveFileDialog();
+
+                                sf.Filter = "Текстовый файл (*.txt)|*.txt";
+                                sf.FilterIndex = 1;
+                                sf.RestoreDirectory = true;
+
+                                if (sf.ShowDialog() == DialogResult.Cancel) return;
+
+                                string filename = sf.FileName;
+                                File.WriteAllText(filename, del);
+                                MessageBox.Show("Файл сохранен");
+                            }
+                            else
+                            {
+                                int i1 = 0;
+                                while (i1 != int.Parse(textBox1.Text))
+                                {
+                                    del += chisla.Dequeue().ToString() + Environment.NewLine;
+                                    i1++;
+                                }
+                                richTextBox1.Clear();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Данный элемент отстутствует!");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Некорректный ввод!");
+                        return;
+                    }
+                    textBox1.Clear();
+                }
+
+
+                richTextBox1.Clear();
+                int[] maschis = new int[chisla.Count];
+                chisla.CopyTo(maschis, 0);
+                for (int i = 0; i < (maschis.Length); i++)
+                {
+                    richTextBox1.Text += (i + 1) + ". " + maschis[i].ToString() + Environment.NewLine;
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Введите число";
+            label1.Visible = true;
+
+            textBox1.Visible = true;
+
+            button1.Text = "Добавить";
+            button1.Visible = true;
+
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Введите номер элемента";
+            label1.Visible = true;
+
+            textBox1.Visible = true;
+
+            button1.Text = "Удалить";
+            button1.Visible = true;
+
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.Cancel) return;
+
+            using (StreamReader sr = new StreamReader(file.FileName, Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    bool success = int.TryParse(line, out int num);
+                    if (success == true)
+                    {
+                        chisla.Enqueue(num);
+
+                        richTextBox1.Clear();
+                        int[] maschis = new int[chisla.Count];
+                        chisla.CopyTo(maschis, 0);
+                        for (int i = 0; i < (maschis.Length); i++)
+                        {
+                            richTextBox1.Text += (i + 1) + ". " + maschis[i].ToString() + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (chisla.Count > 0)
+            {
+                DialogResult yes = MessageBox.Show("Вы действительно хотите очистить очередь?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2, MessageBoxOptions.DefaultDesktopOnly);
+                if (yes == DialogResult.Yes)
+                {
+                    DialogResult sohr = MessageBox.Show("Сохранить удаляемые элементы?", "Выбор", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2, MessageBoxOptions.DefaultDesktopOnly);
+                    if (sohr == DialogResult.Yes)
+                    {
+                        SaveFileDialog sf = new SaveFileDialog();
+
+                        sf.Filter = "Текстовый файл (*.txt)|*.txt";
+                        sf.FilterIndex = 1;
+                        sf.RestoreDirectory = true;
+
+                        if (sf.ShowDialog() == DialogResult.Cancel) return;
+
+                        string filename = sf.FileName;
+                        File.WriteAllText(filename, richTextBox1.Text);
+                        MessageBox.Show("Файл сохранен");
+
+                        richTextBox1.Clear();
+                        chisla.Clear();
+                    }
+                    else
+                    {
+                        richTextBox1.Clear();
+                        chisla.Clear();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Очередь пуста!!!", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (chisla.Count > 0)
+            {
+                SaveFileDialog sf = new SaveFileDialog();
+
+                sf.Filter = "Текстовый файл (*.txt)|*.txt";
+                sf.FilterIndex = 1;
+                sf.RestoreDirectory = true;
+
+                if (sf.ShowDialog() == DialogResult.Cancel) return;
+
+                string filename = sf.FileName;
+                File.WriteAllText(filename, richTextBox1.Text);
+                MessageBox.Show("Файл сохранен");
+            }
+            else
+            {
+                MessageBox.Show("Очередь пуста!!!", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = null;
+            textBox2.ForeColor = Color.Black;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            textBox2.Text = "Строка поиска";
+            textBox2.ForeColor = Color.Gray;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // запуск поиска и ввод только цифр
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 127 && number != 8)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                timer1.Stop();
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // механизм поиска
+            timer1.Stop();
+            timer1.Enabled = false;
+
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+
+            string num1 = textBox2.Text;
+            string num2;
+            int vhod;
+
+            Queue<int> chisla2 = new Queue<int>(chisla);
+
+            for (int i = 0; i < chisla.Count; i++)
+            {
+                num2 = chisla2.Dequeue().ToString();
+                vhod = num2.IndexOf(num1);
+                int index;
+                if (vhod == 0)
+                {
+                    index = richTextBox1.Find(" " + num2);
+                    richTextBox1.Select(index + 1, num1.Length);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                }
+            }
+        }
+
+        int i = 2;
+
+
+        private void sort_mb()
+        {
+            Queue<int> sort = new Queue<int>();
+            Queue<int> chisla1 = new Queue<int>(chisla);
+            int i = 1;
+            int znach = 0;
+
+            while (sort.Count < chisla.Count)
+            {
+                if (i == 1)
+                {
+                    znach = int.MaxValue;
+                    for (int a = 0; a < chisla1.Count; a++)
+                    {
+                        if (chisla1.Peek() < znach)
+                        {
+                            znach = chisla1.Peek();
+                        }
+                        chisla1.Enqueue(chisla1.Dequeue());
+                    }
+                    i = 2;
+                }
+                else if (i == 2)
+                {
+                    for (int a = 0; a < chisla1.Count; a++)
+                    {
+                        if (chisla1.Peek() == znach)
+                        {
+                            sort.Enqueue(chisla1.Dequeue());
+                        }
+                        if (chisla1.Count != 0)
+                        {
+                            chisla1.Enqueue(chisla1.Dequeue());
+                        }
+                    }
+                    i = 1;
+                }
+            }
+
+            richTextBox1.Clear();
+            chisla = sort;
+            int num = 1;
+            foreach (int sr in chisla)
+            {
+                richTextBox1.Text += (num) + ". " + sr.ToString() + Environment.NewLine;
+                num++;
+            }
+        }
+
+        private void sort_bm()
+        {
+            Queue<int> sort = new Queue<int>();
+            Queue<int> chisla1 = new Queue<int>(chisla);
+            int i = 1;
+            int znach = 0;
+
+            while (sort.Count < chisla.Count)
+            {
+                if (i == 1)
+                {
+                    znach = int.MinValue;
+                    for (int a = 0; a < chisla1.Count; a++)
+                    {
+                        if (chisla1.Peek() > znach)
+                        {
+                            znach = chisla1.Peek();
+                        }
+                        chisla1.Enqueue(chisla1.Dequeue());
+                    }
+                    i = 2;
+                }
+                else if (i == 2)
+                {
+                    for (int a = 0; a < chisla1.Count; a++)
+                    {
+                        if (chisla1.Peek() == znach)
+                        {
+                            sort.Enqueue(chisla1.Dequeue());
+                        }
+                        if (chisla1.Count != 0)
+                        {
+                            chisla1.Enqueue(chisla1.Dequeue());
+                        }
+                    }
+                    i = 1;
+                }
+            }
+
+            richTextBox1.Clear();
+            chisla = sort;
+            int num = 1;
+            foreach (int sr in chisla)
+            {
+                richTextBox1.Text += (num) + ". " + sr.ToString() + Environment.NewLine;
+                num++;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (i % 2 == 0)
+            {
+                button5.BackgroundImage = Properties.Resources.меньше_больше;
+                sort_mb();
+            }
+            else
+            {
+                button5.BackgroundImage = Properties.Resources.больше_меньше;
+                sort_bm();
+            }
+            i++;
+        }
+    }
+}
