a88452a8cb73719b6c8597f922cc8e714aeadd3e
Degster23 <73150778+Degster23@users.noreply.github.com>
2020-12-29 06:10:48 +0300

    Massive

---
+++ Massive/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Задание_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Вывести в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.Text = "Ввод";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(64, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Рандом";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 64);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.Text = "Удаление";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Очистка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 266);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 28);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Добавить из файла";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(424, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(22, 20);
+            this.button5.TabIndex = 12;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(194, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(252, 250);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 303);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Стек";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Massive/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Задание_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = false;
+            radioButton3.Checked = false;
+            radioButton3.Enabled = false;
+        }
+
+        string[] mas = new string[1];
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                if (textBox1.Text!=null)
+                {
+                    int razmer = mas.Length;
+                    mas[mas.Length-1] = textBox1.Text;
+
+                    Array.Resize(ref mas, razmer+1);
+                    textBox1.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Не введено значение!!!", "Ошибка");
+                }
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                Random rnd = new Random();
+                mas[mas.Length-1] = rnd.Next(0, 1000).ToString();
+
+                int razmer = mas.Length;
+                Array.Resize(ref mas, razmer+1);
+                
+                textBox1.Clear();
+            }
+
+            if (radioButton3.Checked == true)
+            {
+                if (textBox1.Text != null)
+                {
+                    int n = int.Parse(textBox1.Text);
+                    if (n <= mas.Length)
+                    {
+                        DialogResult rez = MessageBox.Show("Вы действительно хотите удалить "+n+" элемент?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                        if (rez == DialogResult.Yes)
+                        {
+                            DialogResult rez2 = MessageBox.Show("Сохранить удаляемый элемент?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                            if (rez2 == DialogResult.Yes)
+                            {
+                                saveFileDialog1.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+                                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                                {
+                                    string filename = saveFileDialog1.FileName;
+                                    string[] mas2 = new string[1];
+                                    mas2[0] = mas[n - 1];
+                                    
+                                    File.WriteAllLines(filename, mas2);
+                                    MessageBox.Show("Файл сохранен");
+
+                                    mas[n - 1] = null;
+                                    for (int i = (n - 1); i < (mas.Length - 1); i++)
+                                    {
+                                        mas[i] = mas[i + 1];
+                                    }
+                                    int razmer = mas.Length;
+                                    Array.Resize(ref mas, razmer - 1);
+                                }
+                            }
+                            else if (rez2 == DialogResult.No)
+                            {
+                                mas[n - 1] = null;
+                                for (int i = (n - 1); i < (mas.Length - 1); i++)
+                                {
+                                    mas[i] = mas[i + 1];
+                                }
+                                int razmer = mas.Length;
+                                Array.Resize(ref mas, razmer - 1);
+                            }
+                        }
+                    }
+                    else if (n > mas.Length)
+                    {
+                        MessageBox.Show("Введенное значение превышает размерность массива!!!", "Ошибка ввода");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Не введено значение!!!", "Ошибка");
+                }
+            }
+            textBox1.Clear();
+            pokaz();
+        }
+
+        private void pokaz()
+        {
+            richTextBox1.Clear();
+            int numer = 1;
+            foreach (string z in mas)
+            {
+                if (z != null)
+                {
+                    richTextBox1.Text += numer + ". " + z.ToString() + Environment.NewLine;
+                    numer++;
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+
+                File.WriteAllLines(filename, mas);
+                MessageBox.Show("Файл сохранен");
+            }
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Text = "Добавить";
+            label1.Text = "Введите число";
+            button4.Visible = true;
+            textBox1.Enabled = true;
+
+            textBox1.Visible = true;
+            label1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Text = "Рандом";
+            button4.Visible = false;
+            textBox1.Enabled = false;
+
+            textBox1.Visible = false;
+            label1.Visible = false;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Text = "Удалить";
+            label1.Text = "Введите номер";
+            button4.Visible = false;
+            textBox1.Enabled = true;
+
+            textBox1.Visible = true;
+            label1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            DialogResult rez = MessageBox.Show("Вы действительно хотите очистить массив?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+            if (rez == DialogResult.Yes)
+            {
+                DialogResult rez2 = MessageBox.Show("Сохранить удаляемые элементы?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                if (rez2 == DialogResult.Yes)
+                {
+                    saveFileDialog1.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string filename = saveFileDialog1.FileName;
+                        File.WriteAllLines(filename, mas);
+                        MessageBox.Show("Файл сохранен");
+                        Array.Clear(mas, 0, mas.Length);
+                        Array.Resize(ref mas, 1);
+                    }
+                }
+                else if (rez2 == DialogResult.No)
+                {
+                    Array.Clear(mas, 0, mas.Length);
+                    Array.Resize(ref mas, 1);
+                }
+            }
+            pokaz();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                int count = File.ReadAllLines(filename).Length;
+                DialogResult rez2 = MessageBox.Show("Сохранить существующие элементы?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                if (rez2 == DialogResult.Yes)
+                {
+                    int len = mas.Length;
+                    Array.Resize(ref mas, len+count);
+
+                    string[] mas2 = new string[count];
+                    mas2 = File.ReadAllLines(filename);
+                    mas2 = mas2.Where(n => !string.IsNullOrEmpty(n)).ToArray();
+                    int o = 0;
+
+                    mas = mas.Union(mas2).ToArray();
+
+                    int len2 = mas.Length;
+                    Array.Resize(ref mas, len2 + 1);
+                }
+                else if (rez2 == DialogResult.No)
+                {
+                    Array.Clear(mas, 0, mas.Length);
+                    Array.Resize(ref mas, count);
+
+                    mas = File.ReadAllLines(filename);
+                    mas = mas.Where(n => !string.IsNullOrEmpty(n)).ToArray();
+                }
+            }
+            pokaz();
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = null;
+            textBox2.ForeColor = Color.Black;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            textBox2.Text = "Строка поиска";
+            textBox2.ForeColor = Color.Gray;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 127 && number != 8)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                timer1.Stop();
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+
+            string num1 = textBox2.Text;
+            string num2;
+            int vhod;
+
+            for (int i = 0; i < mas.Length-1; i++)
+            {
+                num2 = mas[i].ToString();
+                vhod = num2.IndexOf(num1);
+                int index;
+                if (vhod == 0)
+                {
+                    index = richTextBox1.Find(" " + num2);
+                    richTextBox1.Select(index + 1, num1.Length);
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                }
+            }
+        }
+
+        int i = 2;
+
+        private void sort_mb()
+        {
+            int[] masiv = new int[mas.Length];
+            int[] sort = new int[mas.Length];
+
+            for (int i = 0; i < mas.Length-1; i++)
+            {
+                masiv[i] = int.Parse(mas[i]);
+            }
+
+            int znach = int.MaxValue;
+            int id = 0;
+            for (int i = 0; i < sort.Length; i++)
+            {
+                for (int i2 = 0; i2 < masiv.Length; i2++)
+                {
+                    if (masiv[i2]<znach && masiv[i2]!=0)
+                    {
+                        znach = masiv[i2];
+                        id = i2;
+                    }
+                }
+                masiv[id] = 0;
+                sort[i] = znach;
+                znach = int.MaxValue;
+            }
+
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                mas[i] = sort[i].ToString();
+            }
+
+            pokaz();
+        }
+
+        private void sort_bm()
+        {
+            int[] masiv = new int[mas.Length];
+            int[] sort = new int[mas.Length];
+
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                masiv[i] = int.Parse(mas[i]);
+            }
+
+            int znach = int.MinValue;
+            int id = 0;
+            for (int i = 0; i < sort.Length; i++)
+            {
+                for (int i2 = 0; i2 < masiv.Length; i2++)
+                {
+                    if (masiv[i2] > znach && masiv[i2] != 0)
+                    {
+                        znach = masiv[i2];
+                        id = i2;
+                    }
+                }
+                masiv[id] = 0;
+                sort[i] = znach;
+                znach = int.MinValue;
+            }
+
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                mas[i] = sort[i].ToString();
+            }
+
+            pokaz();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (i % 2 == 0)
+            {
+                button5.BackgroundImage = Properties.Resources.меньше_больше;
+                sort_mb();
+            }
+            else
+            {
+                button5.BackgroundImage = Properties.Resources.больше_меньше;
+                sort_bm();
+            }
+            i++;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 127 && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length < 2)
+            {
+                radioButton3.Enabled = false;
+            }
+            else
+            {
+                radioButton3.Enabled = true;
+            }
+        }
+    }
+}
